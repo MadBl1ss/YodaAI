10f484b3a252ce0f612cbd8cb721c1ba5ef782c4
MadBliss <dan.stepnov@gmail.com>
2024-10-26 10:43:10 +0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,15 @@ import { Analytics } from '@vercel/analytics/react';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
+  metadataBase: new URL('http://localhost:3000'),
   title: "Yoda AI",
   description:
-    "Yoda is an AI bot that speaks in the wise and cryptic style of Master Yoda, from Star Wars"
+    "Yoda is an AI bot that speaks in the wise and cryptic style of Master Yoda, from Star Wars",
+    openGraph: {
+      title: 'Yoda AI',
+      description: 'Yoda is an AI bot that speaks in the wise and cryptic style of Master Yoda, from Star Wars',
+      image: '/app/opengraph-image.png'
+    },
 }
 
 export default function RootLayout({
